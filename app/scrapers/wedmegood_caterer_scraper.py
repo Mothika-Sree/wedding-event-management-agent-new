5a628e16006e6f5aeebd2500120685038e64563b
MOTHIKA SREE R <mothikasree01@gmail.com>
2026-06-28 21:47:58 +0530

    Update wedmegood_caterer_scraper.py

--- app/scrapers/wedmegood_caterer_scraper.py
+++ app/scrapers/wedmegood_caterer_scraper.py
@@ -7,7 +7,14 @@ class WedMeGoodCatererScraper:
 
         with sync_playwright() as p:
 
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(
+                headless=True,
+                args=[
+                    "--no-sandbox",
+                    "--disable-setuid-sandbox",
+                    "--disable-dev-shm-usage"
+                ]
+            )
 
             page = browser.new_page()
 
@@ -18,6 +25,13 @@ class WedMeGoodCatererScraper:
             )
 
             page.wait_for_timeout(5000)
+            print("TITLE:", page.title())
+            print("URL:", page.url)
+            page.screenshot(path="debug.png")
+
+            cards = page.locator("div.vendor-card")
+
+            print("TOTAL:", cards.count())
             vendor_links = []
 
             vendors = page.locator("a.vendor-detail")
